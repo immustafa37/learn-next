21851dacb664617ed449b1135ff09cb42736b4a0
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-12 19:58:56 -0700

    6 - parameter passing in function

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  const apple = ()=>{
-    alert("Fruit")
+  const apple = (item)=>{
+    alert(item)
   }
   return (
     <div className={styles.page}>
       <h1>home</h1>
-      <button onClick={apple}>Click Me</button>
+      <button onClick={()=>apple("Fruit")}>Click Me</button>
     </div>
   );
 }
