1bca8237bb791ce658724fd2bc2813eb22438759
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-12 19:56:01 -0700

    5 - Function

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,13 @@
 import styles from "./page.module.css";
 
 export default function Home() {
+  const apple = ()=>{
+    alert("Fruit")
+  }
   return (
     <div className={styles.page}>
       <h1>home</h1>
-      <button onClick={()=>{
-        alert("Hello Next js")
-      }}>Click Me</button>
+      <button onClick={apple}>Click Me</button>
     </div>
   );
 }
